99e15966cdd1feb96d963097438f2ce4faf7fcab
Vipin Ashodhiya <ashodhiyavipin@gmail.com>
2020-02-20 18:39:36 +0530

    Added database Script and few folders

---
+++ database/database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS 'components';
+USE 'components';
+CREATE TABLE IF NOT EXISTS 'solar_panels' (
+    'serialno' int(10) not null,
+    'brand' varchar(40) not null,
+    ''
+)
