903fa92fd1757686ad9112588d721ac399f278df
ashodhiyavipin@gmail.com <ashodhiyavipin@gmail.com>
2020-02-21 11:51:08 +0530

    Deleted Database for creating a login form

--- database/database.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS 'components';
-USE 'components';
-CREATE TABLE IF NOT EXISTS 'solar_panels' (
-    'serialno' int(10) not null,
-    'brand' varchar(40) not null,
-    ''
-)
